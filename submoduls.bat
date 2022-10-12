378a81a81af7ed216d7af30527afc1d8c437b3ef
Marcel Siegwart <marcel.siegwart@gmx.ch>
2022-10-12 21:46:51 +0200

    add miapplicatio as submodul

--- submoduls.bat
+++ submoduls.bat
@@ -1,6 +1,7 @@
-#adds all needed macchina imaginis repos as submodules
+rem adds all needed macchina imaginis repos as submodules
 git submodule add https://github.com/SigiMcArcel/misound.git
 git submodule add https://github.com/SigiMcArcel/miutils.git
 git submodule add https://github.com/SigiMcArcel/midriver.git
 git submodule add https://github.com/SigiMcArcel/miio.git
-git submodule add https://github.com/SigiMcArcel/miapplication.git
+git submodule add -f https://github.com/SigiMcArcel/miapplication.git
+pause
