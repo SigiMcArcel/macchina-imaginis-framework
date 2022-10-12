d435c44ad813ea0daeb8607fe263f9747867ab31
Marcel Siegwart <marcel.siegwart@gmx.ch>
2022-10-12 21:31:05 +0200

    miapplication added

--- submoduls.bat
+++ submoduls.bat
@@ -3,3 +3,4 @@ git submodule add https://github.com/SigiMcArcel/misound.git
 git submodule add https://github.com/SigiMcArcel/miutils.git
 git submodule add https://github.com/SigiMcArcel/midriver.git
 git submodule add https://github.com/SigiMcArcel/miio.git
+git submodule add https://github.com/SigiMcArcel/miapplication.git
