a1c1b78cbfca8b7c88366be7268dd258d3105724
Marcel Siegwart <marcel.siegwart@gmx.ch>
2023-09-16 10:04:08 +0200

    add mimodules

--- submoduls.bat
+++ submoduls.bat
@@ -3,5 +3,6 @@ git submodule add https://github.com/SigiMcArcel/misound.git
 git submodule add https://github.com/SigiMcArcel/miutils.git
 git submodule add https://github.com/SigiMcArcel/midriver.git
 git submodule add https://github.com/SigiMcArcel/miio.git
+git submodule add https://github.com/SigiMcArcel/mimodules.git
 git submodule add -f https://github.com/SigiMcArcel/miapplication.git
 pause
