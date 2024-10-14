4d970973d0d76de94b7e4c4416286934feaf5fa2
Marcel Siegwart <marcel.siegwart@gmx.ch>
2024-10-14 10:09:46 +0200

    add miTasks

--- submoduls.bat
+++ submoduls.bat
@@ -6,4 +6,5 @@ git submodule add https://github.com/SigiMcArcel/miio.git
 git submodule add https://github.com/SigiMcArcel/mimodules.git
 git submodule add -f https://github.com/SigiMcArcel/miapplication.git
 git submodule add -f https://github.com/SigiMcArcel/micomponents.git
+git submodule add -f https://github.com/SigiMcArcel/mitasks.git
 pause
